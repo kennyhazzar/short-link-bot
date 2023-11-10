52f588368d1225e6f78e7242cf1b783fc4f76cf1
kolchanov <56494597+kennyhazzar@users.noreply.github.com>
2023-11-10 19:49:08 +0700

    add X-Real-IP header

--- src/modules/links/links.controller.ts
+++ src/modules/links/links.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Ip,
-  Logger,
-  Param,
-  Redirect,
-  Req,
-} from '@nestjs/common';
+import { Controller, Get, Logger, Param, Redirect, Req } from '@nestjs/common';
 import { LinksService } from './links.service';
 import { InjectQueue } from '@nestjs/bull';
 import { Queue } from 'bull';
@@ -30,15 +22,12 @@ export class LinksController {
   @ApiExcludeEndpoint()
   @Get(':alias')
   @Redirect()
-  async redirect(
-    @Param('alias') alias: string,
-    @Req() request: Request,
-    @Ip() ip: string,
-  ) {
+  async redirect(@Param('alias') alias: string, @Req() request: Request) {
     this.logger.log(`redirecting: ${alias}`);
 
     const link = await this.linksService.getById(alias);
     const userAgent = request.headers['user-agent'];
+    const ip = request.headers['X-Real-IP'] as unknown as string;
 
     if (!link) {
       const { url } = this.configService.get<TelegrafConfigs>('tg');
